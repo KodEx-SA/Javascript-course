27d7fdf0e73a41046f284d0bff8f31c5684ad8be
Ashley K Motsie <motsieashley31@gmail.com>
2025-09-14 17:29:27 +0200

    Initial commit

---
+++ JS-beginner/object-literal.js
@@ -0,0 +1,19 @@
+const obj = {
+    property_1: {
+        location: "South Africa - JHB",
+            property_details: {
+            bedrooms_number: 5,
+            garage_number: 2,
+            bathrooms: 3
+        }
+    },
+    property_2: {
+        location: "South Africa - CPT",
+            property_details: {
+            bedrooms_number: 5,
+            garage_number: 2,
+            bathrooms: 3
+        }
+    }
+}
+console.log(obj.property_1.location);
---
+++ JS-beginner/template-literals.js
@@ -0,0 +1,9 @@
+let $first_word = "KodEx";
+let $sec_word = "-SA";
+
+var _fullWord = `${$first_word}${$sec_word}`;
+console.log("Hello");
+console.log(_fullWord);
+
+let example = 'Hello\n' + 'World'
+console.log(example);
