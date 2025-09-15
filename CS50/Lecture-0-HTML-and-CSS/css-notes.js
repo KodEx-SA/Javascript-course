97b9fbed0da24ebe561b62257b5e23b695ee4fa9
Ashley K Motsie <motsieashley31@gmail.com>
2025-09-15 20:33:20 +0200

    changing style with javascript

---
+++ CS50/Lecture-0-HTML-and-CSS/css-notes.js
@@ -0,0 +1,7 @@
+var heading = document.getElementById('heading');
+var para = document.getElementById('para');
+
+heading.style.color = '#0ffff';
+heading.style.textAlign = 'center'
+para.style.color = '#00ff';
+para.style.textAlign = 'center'
